208a64351de33da9d7e96838c350f8593faec739
Diego Matos <diegovasconcelos62@gmail.com>
2021-10-22 20:20:31 -0300

    Atualizando Experiencias

--- front-end/src/pages/home/partials/Sobre.jsx
+++ front-end/src/pages/home/partials/Sobre.jsx
@@ -5,8 +5,14 @@ export default function Sobre() {
         <h2 className="title">Sobre Mim</h2>
         <hr />
         <p>
-          Olá, meu nome é <strong>Diego Vasconcelos Schardosim de Matos</strong>, sou  <strong>estudante</strong> de <strong>Ciência da Computação</strong> na <strong>UFRJ</strong> e um entusiasta por jogos. Meu conhecimento Web veio atráves da necessidade de <strong>lecionar aulas</strong> de <strong>HTML</strong>,<strong>CSS</strong> e <strong>JavaScript</strong> moderno utilizando os jogos como meio de aprendizagem em alguns cursos de tecnologia, abrangendo áreas de <strong>Front-End</strong> e <strong>Back-End</strong>. Ainda sou um novato mas estarei sempre me empenhando em aprender e melhorar.
-          </p>
+          Olá, meu nome é <strong>Diego Vasconcelos Schardosim de Matos</strong>, sou estudante de <strong>Ciência da Computação</strong> na <strong>UFRJ</strong> e um entusiasta por jogos.
+        </p>
+        <p>
+          Comecei <strong>lecionando</strong> aulas de <strong>desenvolvimento Web</strong> utilizando os jogos como meio de aprendizagem em alguns cursos de tecnologia, abrangendo áreas de <strong>Front-End</strong> e <strong>Back-End</strong>.
+        </p>
+        <p>
+          Atualmente estou realizando uma <strong>Iniciação Científica</strong> na <a href="http://" target="_blank" rel="noopener noreferrer"></a> <a href="http://www.coppetec.coppe.ufrj.br/site/">COPPETEC</a> e sou membro da <a href="https://gdpufrj.itch.io/">GDP UFRJ</a>. Ainda sou um novato mas estarei sempre me empenhando em aprender e melhorar.
+        </p>
       </div>
     </section>
   );
