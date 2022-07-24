077c15abe21a5df3851eef8f78e784d6be061c52
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-24 17:47:30 -0300

    Adicionando tipo de retorno e corrigindo indentação

--- front-end/seo/global.tsx
+++ front-end/seo/global.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function global() {
+export default function global(): JSX.Element {
     return (
         <Head>
             {/* SEO Geral */}
@@ -13,7 +13,7 @@ export default function global() {
             {/* Google+ / Schema.org */}
             <meta key="schemaTitle" itemProp="name" content="Diego Matos | Desenvolvedor Web" />
             <meta key="schemaDescription" itemProp="description" content="Página principal do desenvolvedor Diego Matos, conheça um pouco sobre ele, entre em contato, baixe seu curriculum ou olhe o código desse site em sua página do GitHub." />
-            <meta itemProp="image" content="imagens/foto-perfil.png" />
+            <meta itemProp="image" content="/assets/imagens/foto-perfil.png" />
             {/* <link href="https://plus.google.com/+SuaPagina" rel="publisher"/> */}
 
             {/* Open Graph Facebook */}
@@ -22,7 +22,7 @@ export default function global() {
             <meta property="og:url" content="https://devdiegomatos.com.br/" />
             <meta property="og:site_name" content="Portfolio do desenvolvedor Diego Matos" />
             <meta property="og:type" content="website" />
-            <meta property="og:image" content="imagens/foto-perfil.png" />
+            <meta property="og:image" content="/assets/imagens/foto-perfil.png" />
             <meta property="og:image:alt" content="Foto do Desenvolvedor Diego Matos" />
             <meta property="og:locale" content="pt-BR" />
 
@@ -31,7 +31,7 @@ export default function global() {
             <meta key="twitterDescription" name="twitter:description" content="Página principal do desenvolvedor Diego Matos, conheça um pouco sobre ele, entre em contato, baixe seu curriculum ou olhe o código desse site em sua página do GitHub." />
             <meta name="twitter:url" content="https://devdiegomatos.com.br/" />
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:image" content="imagens/foto-perfil.png" />
+            <meta name="twitter:image" content="/assets/imagens/foto-perfil.png" />
             {/* <meta name="twitter:creator" content="@estevanmaito"/> */}
             {/* <meta name="twitter:site" content="@empresa"/> */}
         </Head>
