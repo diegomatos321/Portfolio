dca5f4c18adad92bc23ea0899e4bd27e3fbdc7fd
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-24 17:54:15 -0300

    Refatorando template do cabeçalho para nextjs

--- front-end/components/templates/Header.tsx
+++ front-end/components/templates/Header.tsx
@@ -1,55 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import Logo from "../uploads/imagens/logo.webp";
-import { logo } from "../asset-pack.json"
+import Link from "next/link";
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
     <header id="header" className="header">
-      <Link to="/">
+      <Link href="/">
         <picture className="img logo">
-          <source srcSet={logo.webp} type="image/webp"/>
-          <img src={logo.png} alt="Logo do site" title="Logo do Site" />
+          <source srcSet="/assets/imagens/logo.webp" type="image/webp"/>
+          <img src="/assets/imagens/logo.png" alt="Logo do site" title="Logo do Site" />
         </picture>
       </Link>
       <nav className="main-nav">
         <ul className="nav">
           <li className="nav-item item-home">
-            <Link to="/">Home</Link>
+            <Link href="/">Home</Link>
           </li>
 
           <li className="nav-item item-about">
-            <Link to={{
-              pathname:"/",
-              hash:"#about"
-            }}>Sobre Mim</Link>
+            <Link href="/#about">Sobre Mim</Link>
           </li>
 
           <ul className="nav-item sub-nav item-trabalhos">
             <li>
-              <Link to="/trabalhos">Trabalhos</Link>
+              <Link href="/trabalhos">Trabalhos</Link>
             </li>
             <ul className="drop-down">
               <li className="sub-nav-item">
                 <a href="https://gdpufrj.itch.io/laje-4" target="_blank" rel="noopener noreferrer">Lia na Floresta Mágica</a>
               </li>
               <li className="sub-nav-item">
-                <Link to="/trabalhos/flappybird">
+                <Link href="/trabalhos/flappybird">
                   FlappyBird
                 </Link>
               </li>
               <li className="sub-nav-item">
-                <Link to="/trabalhos/jogo-da-nave">
+                <Link href="/trabalhos/jogo-da-nave">
                   Jogo da Nave
                 </Link>
               </li>
               <li className="sub-nav-item">
-                <Link to="/trabalhos/super-mario">
+                <Link href="/trabalhos/super-mario">
                   Super Mario
                 </Link>
               </li>
               <li className="sub-nav-item">
-                <Link to="/trabalhos/jogo-asteroides">
+                <Link href="/trabalhos/jogo-asteroides">
                   Asteroides
                 </Link>
               </li>
@@ -57,7 +51,7 @@ export default function Header() {
           </ul>
 
           <li className="nav-item item-contato">
-            <Link to="/contato">Contato</Link>
+            <Link href="/contato">Contato</Link>
           </li>
         </ul>
       </nav>
