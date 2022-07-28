308edb7c995cc652c760461fefd4806821e285cf
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-28 10:19:33 -0300

    Refatorando cabeçalho do site

--- front-end/components/templates/Header.tsx
+++ front-end/components/templates/Header.tsx
@@ -13,44 +13,13 @@ export default function Header(): JSX.Element {
       </Link>
       <nav className="main-nav">
         <ul className="nav">
-          <li className="nav-item item-home">
-            <Link href="/"><a>Home</a></Link>
-          </li>
-
           <li className="nav-item item-about">
             <Link href="/#about"><a>Sobre Mim</a></Link>
           </li>
 
-          <ul className="nav-item sub-nav item-trabalhos">
-            <li>
-              <Link href="/trabalhos"><a>Trabalhos</a></Link>
-            </li>
-            <ul className="drop-down">
-              <li className="sub-nav-item">
-                <a href="https://gdpufrj.itch.io/laje-4" target="_blank" rel="noopener noreferrer">Lia na Floresta Mágica</a>
-              </li>
-              <li className="sub-nav-item">
-                <Link href="/trabalhos/flappybird">
-                  <a>FlappyBird</a>
-                </Link>
-              </li>
-              <li className="sub-nav-item">
-                <Link href="/trabalhos/jogo-da-nave">
-                  <a>Jogo da Nave</a>
-                </Link>
-              </li>
-              <li className="sub-nav-item">
-                <Link href="/trabalhos/super-mario">
-                  <a>Super Mario</a>
-                </Link>
-              </li>
-              <li className="sub-nav-item">
-                <Link href="/trabalhos/jogo-asteroides">
-                  <a>Asteroides</a>
-                </Link>
-              </li>
-            </ul>
-          </ul>
+          <li className="nav-item item-trabalhos">
+            <Link href="/#trabalhos"><a>Trabalhos</a></Link>
+          </li>
 
           <li className="nav-item item-contato">
             <Link href="/contato"><a>Contato</a></Link>
