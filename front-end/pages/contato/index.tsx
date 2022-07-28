a14b21e9e5e1832fff92cf3f7a4679f316a93e39
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-28 10:19:57 -0300

    Adicionando lógica para enviar requisição de contato para api

--- front-end/pages/contato/index.tsx
+++ front-end/pages/contato/index.tsx
@@ -1,6 +1,58 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import MetaDados from "../../seo/contato"
 
+const DEFAULT_SUBJECT_TEXT: string = "Olá Diego, venho pelo seu Portfolio e desejo falar com você!";
+
 export default function Contato(): JSX.Element {
+    const [contactName, setContactName] = useState<string>("");
+    const [contactEmail, setContactEmail] = useState<string>("");
+    const [subject, setSubject] = useState<string>(DEFAULT_SUBJECT_TEXT);
+    const [messageBody, setMessageBody] = useState<string>("");
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+        try {
+            setIsLoading(() => true);
+
+            const body = {
+                contactName,
+                contactEmail,
+                subject,
+                messageBody
+            }
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contato`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            });
+            const responseBody = await response.json();
+
+            setIsLoading(() => false);
+
+            if (response.ok == false) {
+                console.error(responseBody);
+                return;
+            }
+
+            console.log(responseBody);
+            resetForm();
+        } catch (error: any) {
+            console.error(error.message);
+            setIsLoading(() => false);
+        }
+    }
+
+    const resetForm = () => {
+        setContactName(() => "");
+        setContactEmail(() => "");
+        setSubject(() => DEFAULT_SUBJECT_TEXT);
+        setMessageBody(() => "");
+    }
+
     return (
         <>
             <MetaDados />
@@ -10,47 +62,47 @@ export default function Contato(): JSX.Element {
                         <h1 className="title">Entre em contato</h1>
                         <hr />
                         <div className="container-formulario">
-                            <form className="formulario" onSubmit={this.submitHandler}>
+                            <form className="formulario" onSubmit={handleSubmit}>
                                 <p>Digite seu nome:</p>
                                 <input
                                     type="text"
-                                    name="nome"
-                                    id="nome"
-                                    placeholder="Nome Completo"
+                                    placeholder="Digite seu nome completo"
                                     autoFocus
                                     minLength={5}
                                     maxLength={255}
                                     required
+                                    value={contactName}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setContactName(() => event.target.value)}
                                 />
                                 <p>Digite seu e-mail para contato:</p>
                                 <input
                                     type="email"
-                                    name="email"
-                                    id="email"
-                                    placeholder="Email"
+                                    placeholder="Digite seu e-mail para contato"
                                     required
+                                    value={contactEmail}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setContactEmail(() => event.target.value)}
                                 />
                                 <p>Assunto:</p>
                                 <input
                                     type="text"
-                                    name="assunto"
-                                    id="assunto"
                                     placeholder="Assunto"
                                     minLength={5}
                                     maxLength={50}
                                     required
+                                    value={subject}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => setSubject(() => event.target.value)}
                                 />
                                 <p>Mensagem:</p>
                                 <textarea
-                                    name="mensagem"
-                                    id="mensagem"
                                     placeholder="Digite sua mensagem aqui"
                                     cols={30}
                                     rows={8}
                                     minLength={10}
                                     required
+                                    value={messageBody}
+                                    onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setMessageBody(() => event.target.value)}
                                 ></textarea>
-                                <input type="submit" value="Enviar" className="btn" />
+                                <input type="submit" value="Enviar" className="btn" disabled={isLoading} />
                             </form>
                         </div>
                     </div>
