da91f03d100e319f3a6a5dbb4091805d02bd152d
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-29 10:18:37 -0300

    Encapsulando aplicação em StrictMode

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import type { AppProps } from "next/app";
 import Header from "../components/templates/Header"
 import GlobalSEO from "../seo/global";
@@ -5,10 +6,9 @@ import GlobalSEO from "../seo/global";
 import "../css/index.css"
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-    return <>
+    return <StrictMode>
         <GlobalSEO />
         <Header />
         <Component {...pageProps} />
     </StrictMode>
-    </>
 }
