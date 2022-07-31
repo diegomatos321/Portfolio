57fbad8716db73f35fd2c2b892a97277d5ddf671
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-31 20:47:27 -0300

    WIP: Lógica base para envio de email

--- api/public/contato.php
+++ api/public/contato.php
@@ -8,13 +8,17 @@ $contactEmail = $_POST['contactEmail'];
 $subject = $_POST['subject'];
 
 // Enviar Email
+$from = getenv('MAIL_FROM_ADDRESS');
+$fromName = getenv('MAIL_FROM_NAME');
 $to = getenv('MAIL_FROM_ADDRESS');
 
 $message = '<h1>Olá Diego,</h1><p>Me chamo <strong>'. $contactName . '</strong> e desejo conversar com você sobre:</p><p>.' . $messageBody . '</p>';
 
-$headers = 'From: ' . $contactName . ' <' . $contactEmail . '>\r\n';
-$headers .= 'Reply-To: ' . $replyTo . '\r\n';
-$headers .= 'Content-type: text/html\r\n';
+$headers = array(
+    'From' => $fromName . ' <' . $from . '>',
+    'Reply-To' => $contactEmail,
+    'Content-type' => 'text/html; charset=iso-8859-1'
+);
 
 mail($to, $subject, $message, $headers);
 
