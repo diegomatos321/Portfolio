57abc256469f9a04a8a24df8447aaf0f80340d76
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-08-01 08:58:33 -0300

    Finalizando lógica para enviar email

--- api/public/contato.php
+++ api/public/contato.php
@@ -28,22 +28,45 @@ if ($_SERVER['REQUEST_METHOD'] != "POST") {
 // Resgatar inputs
 $inputs = json_decode(file_get_contents('php://input', true));
 
-// Enviar Email
-$from = getenv('MAIL_FROM_ADDRESS');
-$fromName = getenv('MAIL_FROM_NAME');
-$to = getenv('MAIL_FROM_ADDRESS');
+$to = $_ENV['MAIL_ADDRESS'];
+$mail = new PHPMailer(true);
+$mail->setLanguage('pt_br');
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = $_ENV['MAIL_HOST'];                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = $_ENV['MAIL_USERNAME'];                     //SMTP username
+    $mail->Password   = $_ENV['MAIL_PASSWORD'];                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = $_ENV['MAIL_PORT'];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-$message = '<h1>Olá Diego,</h1><p>Me chamo <strong>'. $inputs->contactName . '</strong> e desejo conversar com você sobre:</p><p>.' . $inputs->messageBody . '</p>';
+    //Recipients
+    $mail->setFrom($inputs->contactEmail, $inputs->contactName);
+    $mail->addAddress($to, $_ENV['MAIL_NAME']);     //Add a recipient
+    $mail->addReplyTo($inputs->contactEmail, $inputs->contactName);
 
-$headers = array(
-    'From' => $fromName . ' <' . $from . '>',
-    'Reply-To' => $inputs->contactEmail,
-    'Content-type' => 'text/html; charset=iso-8859-1'
-);
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $inputs->subject;
+    $mail->Body    = '<h1>Olá Diego,</h1><p>Me chamo <strong>'. $inputs->contactName . '</strong> e desejo conversar com você sobre:</p><p>' . $inputs->messageBody . '</p>';
+    $mail->AltBody = 'Olá Diego,\r\nMe chamo '. $inputs->contactName . ' e desejo conversar com você sobre:\r\n' . $inputs->messageBody;
 
-mail($to, $inputs->subject, $inputs->message, $headers);
-
-header('Access-Control-Allow-Origin', '*');
-header('Content-Type: application/json; chatset=UTF-8');
-http_response_code(200);
-echo json_encode($result);
+    $mail->send();
+    
+    header('Access-Control-Allow-Origin', '*');
+    header('Content-Type: application/json; chatset=UTF-8');
+    http_response_code(200);
+    echo json_encode([
+        'Email enviado com sucesso!'
+    ]);
+} catch (\Throwable $th) {
+    header('Access-Control-Allow-Origin', '*');
+    header('Content-Type: application/json; chatset=UTF-8');
+    http_response_code(500);
+    echo json_encode([
+        'Ocorreu um erro',
+        $th->getMessage()
+    ]);
+}
