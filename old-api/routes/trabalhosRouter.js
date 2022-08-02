05e25e7b16b349b369064256b2b71e0856738504
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-08-02 11:55:15 -0300

    Excluindo pasta antiga

--- old-api/routes/trabalhosRouter.js
+++
@@ -1,6 +0,0 @@
-const trabalhosRouter = require('express').Router({ mergeParams: true });
-const flappyBirdRouter = require("./flappybirdRouter.js");
-
-trabalhosRouter.use("/flappybird", flappyBirdRouter);
-
-module.exports = trabalhosRouter;
