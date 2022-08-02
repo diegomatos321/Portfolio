3dfe9cd76c5350605f38f733ae554802be5f5513
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-08-01 23:56:08 -0300

    Estudando casos do PhP Mailer

---
+++ api/public/message.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email para Contato</title>
+</head>
+<body>
+    <h1><strong>Olá Diego,</strong></h1>
+    <p>Me chamo <?php echo $contactName ?> e desejo falar com você sobre:</p>
+    <p><?php echo $messageBody ?></p>
+</body>
+</html>
