2c6ce2d49c67b6c28fb02b012da9d483f83d5903
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-08-15 22:16:20 -0300

    Adicionando GA para o site

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,12 +1,31 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+
 import Header from "../components/templates/Header"
+import GoogleAnalytics from "../components/GoogleAnalytics";
 import GlobalSEO from "../seo/global";
+import * as gtag from '../lib/gtag';
 
 import "../css/index.css"
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = url => {
+            gtag.pageview(url);
+        }
+
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        return () => {
+            router.events.on('routeChangeComplete', handleRouteChange);
+        }
+    }, [router.events]);
+
     return <StrictMode>
+        <GoogleAnalytics />
         <GlobalSEO />
         <Header />
         <Component {...pageProps} />
