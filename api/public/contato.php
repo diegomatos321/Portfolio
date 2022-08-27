f86b4f7a0c5854150cdccfa8157068efd8c24772
diegomatos <diegovasconcelos62@gmail.com>
2022-08-27 11:21:46 -0300

    Restringindo domínio da origin

--- api/public/contato.php
+++ api/public/contato.php
@@ -10,7 +10,7 @@ $dotenv = \Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: ' . $_ENV['APP_NAME']);
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
     exit(0);
@@ -86,7 +86,7 @@ try {
 }
 
 function JsonResponse(array $message, int $status): string {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: ' . $_ENV['APP_NAME']);
     header('Content-Type: application/json; chatset=UTF-8');
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type');
