84a19c71f35982a8761ee52d091d9eab25c12216
diegomatos <diegovasconcelos62@gmail.com>
2022-08-27 10:37:22 -0300

    Corrigindo rodapé

--- front-end/components/templates/Footer.tsx
+++ front-end/components/templates/Footer.tsx
@@ -1,12 +1,12 @@
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export default function Footer(): JSX.Element {
-    return <footer style={{ backgroundColor: 'black', borderTop: '1px solid yellow', paddingBottom: '12vh' }}>
+    return <footer className='footer' style={{ backgroundColor: 'black', borderTop: '1px solid yellow' }}>
         <div style={{ margin: 'auto', maxWidth: '350px', display: 'flex', flexFlow: 'column', flexWrap: 'nowrap' }}>
             <picture>
                 <img src="/assets/imagens/logo-vertical-fundo-preto.png" width='100%' alt="" />
             </picture>
-            <ul style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <ul style={{ display: 'flex', justifyContent: 'space-evenly', listStyle: 'none' }}>
                 <li><a href="https://www.linkedin.com/in/dev-diego-matos/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={32} color='white' /></a></li>
                 <li><a href="https://github.com/diegomatos321" target="_blank" rel="noopener noreferrer"><FaGithub size={32} color='white' /></a></li>
             </ul>
