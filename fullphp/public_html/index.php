bc8b7a62ee06703f3c4c956a68e601beda6e1b08
diegomatos <diegovasconcelos62@gmail.com>
2024-01-07 17:53:47 -0300

    WIP: Adicionando sessão ao site

--- fullphp/public_html/index.php
+++ fullphp/public_html/index.php
@@ -7,6 +7,15 @@ use App\Controllers\HomeController;
 $dotenv = Dotenv\Dotenv::createImmutable('../');
 $dotenv->load();
 
+session_start([
+    'save_path' => realpath($_SERVER['DOCUMENT_ROOT'] . '/../storage/sessions'),
+    'cookie_lifetime' => 60 * 60 * 24 * 365,
+    'cookie_httponly' => true,
+    'cookie_secure' => true,
+    'cookie_samesite' => 'Lax',
+    'use_strict_mode' => true,
+]);
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $controller = new HomeController();
     $response = $controller->index();
@@ -18,3 +27,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (empty($response) === false)
         echo $response;
 }
+
+session_write_close();
